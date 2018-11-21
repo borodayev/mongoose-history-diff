8dc24873e0656c3924de0a43a1cf77741507a178
frankast <borodaev97@gmail.com>
2018-11-21 15:56:11 +0600

    refactor(Post): fix types, remove plugin opts

--- src/examples/Post.js
+++ src/examples/Post.js
@@ -1,13 +1,13 @@
 // @flow
 /* eslint-disable no-param-reassign, func-names */
 
-import mongoose from 'mongoose';
+import mongoose, { type MongooseSchema } from 'mongoose';
 import DiffPlugin from '../index';
 import DB from './db';
 
 DB.init('mongodb://localhost:27017/testdb');
 
-export const PostSchema = new mongoose.Schema(
+export const PostSchema: MongooseSchema<PostDoc> = new mongoose.Schema(
   {
     title: {
       type: String,
@@ -23,9 +23,11 @@ export const PostSchema = new mongoose.Schema(
 
 export class PostDoc /* :: extends Mongoose$Document */ {
   title: string;
+
+  /* :: static diffModel(): any {} */
 }
 
 PostSchema.loadClass(PostDoc);
-PostSchema.plugin(DiffPlugin, { mongooseConnection: DB.data, diffCollectionName: 'post_diff' });
+PostSchema.plugin(DiffPlugin);
 
 export const Post = DB.data.model('Post', PostSchema);
