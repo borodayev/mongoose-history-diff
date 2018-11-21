28fc170e247845233ca2337c675021e03b24003c
frankast <borodaev97@gmail.com>
2018-11-21 15:56:46 +0600

    test(plugin-test): fix types

--- src/examples/__tests__/plugin-test.js
+++ src/examples/__tests__/plugin-test.js
@@ -1,20 +1,19 @@
 // @flow
 
-import { Post } from '../Post';
+import { Post, type PostDoc } from '../Post';
 
 jest.mock('../db');
-
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000000;
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 
 describe('plugin', () => {
   it('create history model', async () => {
     await Post.create({ title: 'new' });
-    const post = await Post.findOne({ title: 'new' });
+    const post: PostDoc = (await Post.findOne({ title: 'new' }).exec(): any);
     post.title = 'updated';
     await post.save();
 
     const Diff = Post.diffModel();
-    const diffs = await Diff.find({});
+    const diffs = await Diff.find({}).exec();
     expect(post.title).toBe('updated');
     expect(Array.isArray(diffs)).toBeTruthy();
     expect(Array.isArray(diffs[0].changes)).toBeTruthy();
