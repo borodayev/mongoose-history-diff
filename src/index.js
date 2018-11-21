0b4dea7e561bee9c12a4ef0eb61c4e098b5b1296
frankast <borodaev97@gmail.com>
2018-11-21 15:54:37 +0600

    refactor(index): get rid of opts, take model from statics

--- src/index.js
+++ src/index.js
@@ -2,18 +2,20 @@
 /* eslint-disable no-param-reassign, func-names */
 
 import deepDiff from 'deep-diff';
-import type { MongooseConnection } from 'mongoose';
+import type { MongooseSchema } from 'mongoose';
 import DiffModel from './Diff';
 
 export type OptionsT = {|
-  mongooseConnection: MongooseConnection,
-  diffCollectionName: string,
+  // mongooseConnection: ?MongooseConnection,
+  diffCollectionName: ?string,
 |};
 
-export default function plugin(schema: any, options: OptionsT) {
-  const Diff = DiffModel(options);
-
-  schema.statics.diffModel = () => Diff;
+export default function plugin(schema: MongooseSchema<any>, options: ?OptionsT) {
+  // $FlowFixMe
+  schema.static('diffModel', function() {
+    const collectionName = options?.diffCollectionName || `${this.collection.name}_diff`;
+    return DiffModel(this.collection.conn, collectionName);
+  });
 
   schema.post('init', function() {
     this._original = this.toObject();
@@ -23,6 +25,7 @@ export default function plugin(schema: any, options: OptionsT) {
     if (!this.isNew) {
       const lhs = this._original;
       const rhs = this.toObject();
+      const Diff = this.constructor.diffModel();
       const diffs = deepDiff.diff(lhs, rhs);
       if (diffs?.length > 0) await Diff.createDiff(lhs._id, diffs);
       this._original = null;
