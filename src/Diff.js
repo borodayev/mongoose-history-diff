01f354c663b89b4ca368c421fcf8f4eefab99b34
frankast <borodaev97@gmail.com>
2018-11-26 18:31:03 +0600

    wip

--- src/Diff.js
+++ src/Diff.js
@@ -1,4 +1,5 @@
 // @flow
+/* eslint-disable no-await-in-loop */
 
 import { Schema, type ObjectId, type MongooseModel, type MongooseConnection } from 'mongoose';
 
@@ -35,7 +36,6 @@ export default function(
         type: String,
         enum: ['E', 'N', 'D', 'A'],
       },
-      path: [String],
       lhs: Schema.Types.Mixed,
       rhs: Schema.Types.Mixed,
       index: Number,
@@ -44,16 +44,18 @@ export default function(
     {
       _id: false,
       versionKey: false,
+      timestamps: true,
     }
   );
 
   class ChangeDoc /* :: extends Mongoose$Document */ {
     kind: 'E' | 'N' | 'D' | 'A';
-    path: Array<string>;
     lhs: any;
     rhs: any;
     index: ?number;
     item: ?ItemDoc;
+    createdAt: Date;
+    updatedAt: Date;
   }
 
   ChangeSchema.loadClass(ChangeDoc);
@@ -61,6 +63,7 @@ export default function(
   const DiffSchema = new Schema(
     {
       docId: Schema.Types.ObjectId,
+      path: [String],
       changes: [ChangeSchema],
     },
     { versionKey: false, timestamps: true, collection: collectionName }
@@ -70,11 +73,26 @@ export default function(
     // $FlowFixMe
     _id: ObjectId;
     docId: ObjectId;
+    path: Array<string>;
     changes: Array<ChangeDoc>;
 
-    static async createDiff(docId: ObjectId, changes: Array<ChangeDoc>): Promise<DiffDoc> {
-      const doc = new this({ docId, changes });
-      return doc.save();
+    static async createOrUpdateDiffs(docId: ObjectId, changes: Array<Object>): Promise<void> {
+      for (const change of changes) {
+        let doc;
+        const path = change.path;
+        delete change.path;
+        doc = await this.findOne({ path }).exec();
+        if (doc) {
+          doc.changes.push(change);
+        } else {
+          doc = new this({ docId, path, changes: [change] });
+        }
+        await doc.save();
+      }
+    }
+
+    static async findAllByDocId(docId: ObjectId): Promise<Array<DiffDoc>> {
+      return this.find({ docId }).exec();
     }
   }
 
